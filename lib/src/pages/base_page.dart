e1c54f651661f9fe77bb022bd33d5d2d48d6b5c9
EduardoGomesSa <eduardo.sa.022@gmail.com>
2024-06-28 22:59:51 -0300

    Criação da class HomePage

--- lib/src/pages/base_page.dart
+++ lib/src/pages/base_page.dart
@@ -6,7 +6,11 @@ class BasePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          
+        ],
+      ),
     );
   }
 }
