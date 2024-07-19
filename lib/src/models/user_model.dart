99dd307556e3ff304aa020cbdf19abf135e3e63b
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-07-18 22:41:26 -0300

    Criação dos outros métodos  de UserModel

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+
 class UserModel {
   int? id;
   String? name;
@@ -13,5 +17,38 @@ class UserModel {
     this.password,
     this.createdAt,
     this.token,
-  })
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'created_at': createdAt?.millisecondsSinceEpoch,
+      'token': token,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id']?.toInt(),
+      name: map['name'],
+      email: map['email'],
+      password: map['password'],
+      createdAt: map['created_at'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['created_at'])
+          : null,
+      token: map['token'],
+    );
+  }
+
+  static List<UserModel> fromList(list) {
+    return List<UserModel>.from(list.map((x) => UserModel.fromMap(x)));
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source));
 }
