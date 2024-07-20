9b476d935b051a532b033ba4d93800c4cb123764
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-07-19 23:07:12 -0300

    Criação do método de registro

--- lib/src/repositories/auth_repository.dart
+++ lib/src/repositories/auth_repository.dart
@@ -38,4 +38,28 @@ class AuthRepository {
       return ApiResult<UserModel>(message: message, isError: true);
     }
   }
+
+  Future<ApiResult<UserModel>> signUp(UserModel user) async {
+    const String endpoint = "${Url.base}/register";
+
+    Map<String, dynamic> body = user.toMap();
+    body['password_confirmation'] = user.password;
+
+    final response = await httpManager.request(
+      url: endpoint,
+      method: HttpMethods.post,
+      body: body,
+    );
+
+    if (response['data'] != null) {
+      UserModel user = UserModel.fromMap(response['data']);
+
+      return ApiResult<UserModel>(data: user);
+    } else {
+      String message = response['error'] ??
+          'Não foi possível fazer o cadastro.Tente novamente!';
+
+      return ApiResult<UserModel>(message: message, isError: true);
+    }
+  }
 }
