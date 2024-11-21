431e318613b8b28000416bf22bfc8df8a9aab073
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-20 23:50:51 -0300

    Ao clicar no card task , redireciona para a taskpage

--- lib/src/core/widgets/task_card_widget.dart
+++ lib/src/core/widgets/task_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mytodolist/src/controllers/task_controller.dart';
 import 'package:mytodolist/src/models/task_model.dart';
+import 'package:mytodolist/src/pages/task/task_page.dart';
 
 class TaskCardWidget extends StatelessWidget {
   const TaskCardWidget({
@@ -16,52 +17,56 @@ class TaskCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-      color: task.status == "ativo"
-          ? Colors.blue
-          : const Color.fromARGB(255, 149, 204, 229),
-      child: ListTile(
-        leading: IconButton(
-          onPressed: () => controller.changeStatus(
-              id: task.id!,
-              status: task.status == "ativo" ? "inativo" : "ativo"),
-          icon: Icon(
-            task.status == "ativo"
-                ? Icons.check_box_outline_blank_sharp
-                : Icons.check_box_outlined,
-            size: 30,
-          ),
-          color: Colors.white,
-        ),
-        title: Text(
-          task.name.toString(),
-          style: TextStyle(
-              color: task.status == "ativo" ? Colors.black : Colors.black38,
-              decoration: task.status == "ativo"
-                  ? TextDecoration.none
-                  : TextDecoration.lineThrough),
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            task.status == "ativo"
-                ? IconButton(
-                    onPressed: onShowModalEditTask,
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.black,
-                    ))
-                : const SizedBox.shrink(),
-            const SizedBox(width: 10),
-            IconButton(
-              onPressed: () => controller.delete(id: task.id!),
-              icon: Icon(
-                Icons.delete,
-                color: task.status == "ativo" ? Colors.black : Colors.black38,
-              ),
+    return GestureDetector(
+      onTap: () => Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => TaskPage())),
+      child: Card(
+        margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+        color: task.status == "ativo"
+            ? Colors.blue
+            : const Color.fromARGB(255, 149, 204, 229),
+        child: ListTile(
+          leading: IconButton(
+            onPressed: () => controller.changeStatus(
+                id: task.id!,
+                status: task.status == "ativo" ? "inativo" : "ativo"),
+            icon: Icon(
+              task.status == "ativo"
+                  ? Icons.check_box_outline_blank_sharp
+                  : Icons.check_box_outlined,
+              size: 30,
             ),
-          ],
+            color: Colors.white,
+          ),
+          title: Text(
+            task.name.toString(),
+            style: TextStyle(
+                color: task.status == "ativo" ? Colors.black : Colors.black38,
+                decoration: task.status == "ativo"
+                    ? TextDecoration.none
+                    : TextDecoration.lineThrough),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              task.status == "ativo"
+                  ? IconButton(
+                      onPressed: onShowModalEditTask,
+                      icon: const Icon(
+                        Icons.edit,
+                        color: Colors.black,
+                      ))
+                  : const SizedBox.shrink(),
+              const SizedBox(width: 10),
+              IconButton(
+                onPressed: () => controller.delete(id: task.id!),
+                icon: Icon(
+                  Icons.delete,
+                  color: task.status == "ativo" ? Colors.black : Colors.black38,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:mytodolist/src/controllers/task_controller.dart';
 import 'package:mytodolist/src/core/widgets/task_add_modal.dart';
