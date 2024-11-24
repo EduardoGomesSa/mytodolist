3009f0559efd8e599191a9bda3084797fbcec408
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-23 23:54:46 -0300

    Importando classes necessárias no ItemController

--- lib/src/controllers/item_controller.dart
+++ lib/src/controllers/item_controller.dart
@@ -1,7 +1,17 @@
 import 'package:get/get.dart';
+import 'package:mytodolist/src/controllers/auth_controller.dart';
 import 'package:mytodolist/src/models/item_model.dart';
+import 'package:mytodolist/src/repositories/item_repository.dart';
 
 class ItemController extends GetxController {
+  final AuthController auth;
+  final ItemRepository repository;
+
+  ItemController({
+    required this.auth,
+    required this.repository,
+  });
+
   RxBool isLoading = false.obs;
   ItemModel item = ItemModel();
 }
