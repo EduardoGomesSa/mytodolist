b3df5e335cc5b293cb39ca70849e2b5ca6dbf6f5
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-24 17:21:29 -0300

    Criação do método de criar item no ItemController

--- lib/src/controllers/item_controller.dart
+++ lib/src/controllers/item_controller.dart
@@ -3,14 +3,17 @@ import 'package:mytodolist/src/controllers/auth_controller.dart';
 import 'package:mytodolist/src/core/utils/api_result.dart';
 import 'package:mytodolist/src/models/item_model.dart';
 import 'package:mytodolist/src/repositories/item_repository.dart';
+import 'package:mytodolist/src/repositories/task_repository.dart';
 
 class ItemController extends GetxController {
   final AuthController auth;
   final ItemRepository repository;
+  final TaskRepository taskRepository;
 
   ItemController({
     required this.auth,
     required this.repository,
+    required this.taskRepository,
   });
 
   RxBool isLoading = false.obs;
@@ -27,7 +30,7 @@ class ItemController extends GetxController {
     );
 
     if (!result.isError) {
-      // recarrega a task do item
+      await taskRepository.getById(token: token, id: 1);
     }
 
     isLoading.value = false;
