0efbec26daa0eed0e25fc391c655c6a09ebf0243
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-24 00:03:29 -0300

    Criação do método de criar item

--- lib/src/controllers/item_controller.dart
+++ lib/src/controllers/item_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:mytodolist/src/controllers/auth_controller.dart';
+import 'package:mytodolist/src/core/utils/api_result.dart';
 import 'package:mytodolist/src/models/item_model.dart';
 import 'package:mytodolist/src/repositories/item_repository.dart';
 
@@ -14,4 +15,21 @@ class ItemController extends GetxController {
 
   RxBool isLoading = false.obs;
   ItemModel item = ItemModel();
+
+  Future post() async {
+    String token = auth.user.token!;
+
+    isLoading.value = true;
+
+    ApiResult result = await repository.insert(
+      token: token,
+      model: item,
+    );
+
+    if (!result.isError) {
+      // recarrega a task do item
+    }
+
+    isLoading.value = false;
+  }
 }
