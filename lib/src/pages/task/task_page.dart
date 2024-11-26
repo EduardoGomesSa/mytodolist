bd2e2f3501576a8b0bd84cb642b46f7d4b7f4941
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-25 23:47:04 -0300

    Exibição dos dados da task na TaskPage

--- lib/src/pages/task/task_page.dart
+++ lib/src/pages/task/task_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mytodolist/src/controllers/task_controller.dart';
 import 'package:mytodolist/src/core/widgets/item_add_modal.dart';
+import 'package:mytodolist/src/core/widgets/task_card_widget.dart';
 import 'package:mytodolist/src/models/task_model.dart';
 
 class TaskPage extends StatelessWidget {
-  const TaskPage({super.key, required this.model});
+  TaskPage({super.key, required this.model});
 
   final TaskModel model;
+  final TaskController controller = Get.find<TaskController>();
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +19,20 @@ class TaskPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Expanded(child: Text(model.name!)),
+          Card(
+            color: Colors.blue,
+            child: Column(
+              children: [
+                const Text("TAREFA"),
+                TaskCardWidget(
+                    task: model,
+                    controller: controller,
+                    onShowModalEditTask: () {}),
+                Text(model.description ?? ''),
+                Text(model.createdAt.toString()),
+              ],
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
