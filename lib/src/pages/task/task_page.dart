6744df8ac94fe592252465a0034730096ce8119b
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-26 00:03:09 -0300

    organizando todas as informações de Task no seu lugar

--- lib/src/pages/task/task_page.dart
+++ lib/src/pages/task/task_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mytodolist/src/controllers/task_controller.dart';
 import 'package:mytodolist/src/core/widgets/item_add_modal.dart';
-import 'package:mytodolist/src/core/widgets/task_card_widget.dart';
 import 'package:mytodolist/src/models/task_model.dart';
 
 class TaskPage extends StatelessWidget {
@@ -20,60 +19,78 @@ class TaskPage extends StatelessWidget {
       body: Column(
         children: [
           Card(
+            margin: const EdgeInsets.only(top: 15, left: 20, right: 20),
             color: Colors.blue,
-            child: Column(
-              children: [
-                const Text("TAREFA"),
-                ListTile(
-                  leading: IconButton(
-                    onPressed: () => controller.changeStatus(
-                        id: model.id!,
-                        status: model.status == "ativo" ? "inativo" : "ativo"),
-                    icon: Icon(
-                      model.status == "ativo"
-                          ? Icons.check_box_outline_blank_sharp
-                          : Icons.check_box_outlined,
-                      size: 30,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              child: Column(
+                children: [
+                  const Text("TAREFA"),
+                  ListTile(
+                    leading: IconButton(
+                      onPressed: () => controller.changeStatus(
+                          id: model.id!,
+                          status: model.status == "ativo" ? "inativo" : "ativo"),
+                      icon: Icon(
+                        model.status == "ativo"
+                            ? Icons.check_box_outline_blank_sharp
+                            : Icons.check_box_outlined,
+                        size: 30,
+                      ),
+                      color: Colors.white,
                     ),
-                    color: Colors.white,
-                  ),
-                  title: Text(
-                    model.name.toString(),
-                    style: TextStyle(
-                        color: model.status == "ativo"
-                            ? Colors.black
-                            : Colors.black38,
-                        decoration: model.status == "ativo"
-                            ? TextDecoration.none
-                            : TextDecoration.lineThrough),
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      model.status == "ativo"
-                          ? IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.edit,
-                                color: Colors.black,
-                              ))
-                          : const SizedBox.shrink(),
-                      const SizedBox(width: 10),
-                      IconButton(
-                        onPressed: () => controller.delete(id: model.id!),
-                        icon: Icon(
-                          Icons.delete,
+                    title: Text(
+                      model.name.toString(),
+                      style: TextStyle(
                           color: model.status == "ativo"
                               ? Colors.black
                               : Colors.black38,
+                          decoration: model.status == "ativo"
+                              ? TextDecoration.none
+                              : TextDecoration.lineThrough),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        model.status == "ativo"
+                            ? IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.black,
+                                ))
+                            : const SizedBox.shrink(),
+                        IconButton(
+                          onPressed: () => controller.delete(id: model.id!),
+                          icon: Icon(
+                            Icons.delete,
+                            color: model.status == "ativo"
+                                ? Colors.black
+                                : Colors.black38,
+                          ),
                         ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 80),
+                        child: Text(model.description ?? ''),
                       ),
                     ],
                   ),
-                ),
-                Text(model.description ?? ''),
-                Text(model.createdAt.toString()),
-              ],
+                  const SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25),
+                        child: Text(model.createdAt.toString()),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
