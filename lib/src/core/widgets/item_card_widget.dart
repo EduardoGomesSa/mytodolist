bea7f11587b287f7a8698d90792ef4b65878ff89
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-26 23:48:36 -0300

    Criação do layout da checkbox

--- lib/src/core/widgets/item_card_widget.dart
+++ lib/src/core/widgets/item_card_widget.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mytodolist/src/controllers/item_controller.dart';
+import 'package:mytodolist/src/models/item_model.dart';
 
 class ItemCardWidget extends StatelessWidget {
-  const ItemCardWidget({super.key});
+  const ItemCardWidget({
+    super.key,
+    required this.model,
+    required this.controller,
+  });
+
+  final ItemModel model;
+  final ItemController controller;
 
   @override
   Widget build(BuildContext context) {
-    return const Card();
+    return Card(
+      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+      color: model.status == "ativo"
+          ? Colors.blue
+          : const Color.fromARGB(255, 149, 204, 229),
+      child: ListTile(
+        leading: IconButton(
+          onPressed: () => controller.changeStatus(
+              id: model.id!,
+              status: model.status == "ativo" ? "inativo" : "ativo"),
+          icon: Icon(
+            model.status == "ativo"
+                ? Icons.check_box_outline_blank_sharp
+                : Icons.check_box_outlined,
+            size: 30,
+          ),
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
