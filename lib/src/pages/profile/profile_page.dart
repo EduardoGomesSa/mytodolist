33a47ca38221c6d62d6d7ad3f75e8d53e07aa012
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-12-03 23:39:19 -0300

    Criação do layout do botão de sair do app

--- lib/src/pages/profile/profile_page.dart
+++ lib/src/pages/profile/profile_page.dart
@@ -1,16 +1,38 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mytodolist/src/controllers/profile_controller.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.find<ProfileController>();
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Perfil'),
       ),
-      body: const  Column(
-        children: [],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: SizedBox(
+              width: double.infinity,
+              height: 45,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  )
+                ),
+                onPressed: () {}, child: const Text("Sair"),),),
+          ),
+        ],
       ),
     );
   }
