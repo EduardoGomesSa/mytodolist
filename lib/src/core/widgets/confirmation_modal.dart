4a52d04591ee960f0f5fdc78e484aec992e8cc00
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-12-08 21:10:20 -0300

    Criação do arquivo de modal de confirmação

---
+++ lib/src/core/widgets/confirmation_modal.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class ConfirmationModal extends StatelessWidget {
+  const ConfirmationModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
