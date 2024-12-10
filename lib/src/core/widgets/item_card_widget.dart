dd2c7b468a23d76852a75a6b1da0254027abe07a
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-12-09 23:41:09 -0300

    Ajuste layout lista de items

--- lib/src/core/widgets/item_card_widget.dart
+++ lib/src/core/widgets/item_card_widget.dart
@@ -47,57 +47,51 @@ class ItemCardWidget extends StatelessWidget {
           });
     }
 
-    return Card(
-      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-      color: model.status == "ativo"
-          ? Colors.blue
-          : const Color.fromARGB(255, 149, 204, 229),
-      child: ListTile(
-        leading: IconButton(
-          onPressed: () {
-            controller.changeStatus(
-                id: model.id!,
-                taskId: model.taskId!,
-                status: model.status == "ativo" ? "inativo" : "ativo");
-          },
-          icon: Icon(
-            model.status == "ativo"
-                ? Icons.check_box_outline_blank_sharp
-                : Icons.check_box_outlined,
-            size: 30,
-          ),
-          color: Colors.white,
+    return ListTile(
+      leading: IconButton(
+        onPressed: () {
+          controller.changeStatus(
+              id: model.id!,
+              taskId: model.taskId!,
+              status: model.status == "ativo" ? "inativo" : "ativo");
+        },
+        icon: Icon(
+          model.status == "ativo"
+              ? Icons.check_box_outline_blank_sharp
+              : Icons.check_box_outlined,
+          size: 30,
         ),
-        title: Text(
-          model.name.toString(),
-          style: TextStyle(
+        color: model.status == "ativo" ? Colors.black : Colors.black38,
+      ),
+      title: Text(
+        model.name.toString(),
+        style: TextStyle(
+            color: model.status == "ativo" ? Colors.black : Colors.black38,
+            decoration: model.status == "ativo"
+                ? TextDecoration.none
+                : TextDecoration.lineThrough),
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          model.status == "ativo"
+              ? IconButton(
+                  onPressed: onShowModalEditItem,
+                  icon: const Icon(
+                    Icons.edit,
+                    color: Colors.black,
+                  ))
+              : const SizedBox.shrink(),
+          const SizedBox(width: 10),
+          IconButton(
+            onPressed: () =>
+                excluirItem(),
+            icon: Icon(
+              Icons.delete,
               color: model.status == "ativo" ? Colors.black : Colors.black38,
-              decoration: model.status == "ativo"
-                  ? TextDecoration.none
-                  : TextDecoration.lineThrough),
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            model.status == "ativo"
-                ? IconButton(
-                    onPressed: onShowModalEditItem,
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.black,
-                    ))
-                : const SizedBox.shrink(),
-            const SizedBox(width: 10),
-            IconButton(
-              onPressed: () =>
-                  excluirItem(),
-              icon: Icon(
-                Icons.delete,
-                color: model.status == "ativo" ? Colors.black : Colors.black38,
-              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
