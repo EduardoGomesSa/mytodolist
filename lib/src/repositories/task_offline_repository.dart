d57e546e7d3e2724f689a7d3c6a3ef2d0336ece9
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-12-15 21:39:59 -0300

    Criando método de buscar todas as tasks internas

--- lib/src/repositories/task_offline_repository.dart
+++ lib/src/repositories/task_offline_repository.dart
@@ -1,7 +1,23 @@
+import 'package:mytodolist/src/core/utils/api_result.dart';
 import 'package:mytodolist/src/models/task_model.dart';
 import 'package:mytodolist/src/repositories/databases/db.dart';
 
 class TaskOfflineRepository {
+  Future<ApiResult<List<TaskModel>>> getAll() async {
+    final db = await Db.connection();
+
+    var tasks = await db.query('tasks', orderBy: 'created_at DESC');
+
+    if (tasks.isNotEmpty) {
+      List<TaskModel> listTasks = TaskModel.fromList(tasks as List);
+      return ApiResult(data: listTasks);
+    }
+
+    String message =
+          "Não foi possível buscar as tarefas. Tente novamente!";
+      return ApiResult<List<TaskModel>>(message: message, isError: true);
+  }
+
   Future<int> insert(TaskModel model) async {
     final db = await Db.connection();
 
