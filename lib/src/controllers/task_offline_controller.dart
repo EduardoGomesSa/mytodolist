92d06e4c7b6aa0b3c701e73cffec4a0d0e877102
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-12-15 22:45:58 -0300

    Criação do método de buscar todos as tarefas no TaskOfflineController

--- lib/src/controllers/task_offline_controller.dart
+++ lib/src/controllers/task_offline_controller.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import 'package:mytodolist/src/core/utils/api_result.dart';
 import 'package:mytodolist/src/core/utils/app_utils.dart';
+import 'package:mytodolist/src/models/task_model.dart';
 import 'package:mytodolist/src/repositories/task_offline_repository.dart';
 
 class TaskOfflineController extends GetxController {
@@ -11,5 +13,21 @@ class TaskOfflineController extends GetxController {
     required this.appUtils,
   });
 
-  
+  RxBool isLoading = false.obs;
+  Rx<TaskModel> task = TaskModel().obs;
+  RxList<TaskModel> listTask = RxList<TaskModel>([]);
+
+  Future getAll() async {
+    isLoading.value = true;
+
+    ApiResult<List<TaskModel>> result = await repository.getAll();
+
+    if (!result.isError) {
+      listTask.assignAll(result.data!);
+    } else {
+      appUtils.showToast(message: result.message!, isError: result.isError);
+    }
+
+    isLoading.value = false;
+  }
 }
