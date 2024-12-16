0a3427ab134b3bdf8770ca8bc03c42c073558cfb
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-12-15 21:03:07 -0300

    criando método de insert no banco interno

--- lib/src/repositories/task_offline_repository.dart
+++ lib/src/repositories/task_offline_repository.dart
@@ -1,3 +1,34 @@
+import 'package:mytodolist/src/models/task_model.dart';
+import 'package:mytodolist/src/repositories/databases/db.dart';
+
 class TaskOfflineRepository {
-  
+  Future<int> insert(TaskModel model) async {
+    final db = await Db.connection();
+
+    var saved = await db.transaction((txn) async {
+      final taskId = await txn.insert('tasks', {
+        'name': model.name,
+        'description': model.description,
+        'status': 'ativo',
+        'created_at': DateTime.now().toIso8601String(),
+        'updated_at': DateTime.now().toIso8601String(),
+      });
+
+      if (model.items != null && model.items!.isNotEmpty) {
+        for (var item in model.items!) {
+          await txn.insert('items', {
+            'task_id': taskId,
+            'name': item.name,
+            'status': 'ativo',
+            'created_at': DateTime.now().toIso8601String(),
+            'updated_at': DateTime.now().toIso8601String(),
+          });
+        }
+      }
+
+      return taskId > 0 ? 1 : 0;
+    });
+
+    return saved;
+  }
 }
