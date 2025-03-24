56ffcbce7c471a4e78d71d558c53c092a70a13ba
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-03-24 00:27:52 -0300

    feat: :sparkles: Melhoramento da tela de splash

--- lib/src/pages/splash_page.dart
+++ lib/src/pages/splash_page.dart
@@ -1,10 +1,15 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: const BoxDecoration(color: Colors.blue),
+      child: Center(
+        child: Image.asset("assets/images/logo.png"),
+      ),
+    );
   }
 }
